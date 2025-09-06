ebf45cd60bbc0c810eff0413f746220caf590bb8
Kristina <the.frog.1raveler@gmail.com>
2025-09-07 00:16:13 +0600

    Добавлние комментария

--- src/services/orders.js
+++ src/services/orders.js
@@ -1,2 +1,5 @@
-import api from './api'
-export const createOrder= async(p)=> (await api.post('/orders',p)).data
+import api from './api'; // Импортируем модуль api.
+
+export const createOrder = async (p) => {
+  return (await api.post('/orders', p)).data; // Отправляем POST-запрос на создание заказа и возвращаем даные.
+};
